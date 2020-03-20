89c3dcedbe2a769f94b0d9c5c390d0f6470449e1
tgmielke <tmielke@salesforce.com>
2020-03-20 13:10:17 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
-sfdx shane:profile:whitelist -n System Administrator
-sfdx shane:profile:whitelist -n FSL Optimization
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/page/home
