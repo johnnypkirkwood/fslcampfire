2ecc3fa14a6c777d1773a73f3fa7f3d52ba2968d
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-27 18:36:17 -0400

    Update orgInit.sh
    rearranged

--- orgInit.sh
+++ orgInit.sh
@@ -3,8 +3,8 @@ sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
-sfdx force:user:password:generate
 sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
+sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/page/home
