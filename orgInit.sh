03475814b495163f4fa2b7160bec556424121cd3
tgmielke <tmielke@salesforce.com>
2020-03-27 09:40:35 -0700

    Update orgInit.sh
    pull perm sets

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,5 @@
+sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
+sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
