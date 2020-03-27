6250e12bb095cde36f1d96ea4c469adcc4c56d98
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-27 16:56:09 -0400

    Update orgInit.sh
    added more perms

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,9 @@
-
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_License
+sfdx force:source:retrieve -m PermissionSet:FSL_Admin_License
+sfdx force:source:retrieve -m PermissionSet:FSL_Mobile_License
+sfdx force:source:retrieve -m PermissionSet:FSL_Resource_License
+sfdx force:source:retrieve -m PermissionSet:FSL_FSL_Community_Self_Service_Permissions
+sfdx force:source:retrieve -m PermissionSet:FSL_Resource_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
