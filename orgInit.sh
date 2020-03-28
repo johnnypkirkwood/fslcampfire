73fb7fb9ad34230d770a58c72635ebea648db7cd
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-27 20:33:20 -0400

    Update orgInit.sh
    try try try

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_License
 sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
+sfdx force:data:record:get -s FieldServiceMobileSettings -w "Name='Field Service Mobile Settings'"
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n FSL_Dispatcher_License
