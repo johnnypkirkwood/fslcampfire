edbc02d39b1307a11783ec3c946e89bb8b9e5dc1
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-27 20:36:57 -0400

    Update orgInit.sh
    api names

--- orgInit.sh
+++ orgInit.sh
@@ -1,12 +1,12 @@
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_License
 sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
-sfdx force:data:record:get -s FieldServiceMobileSettings -w "Name='Field Service Mobile Settings'"
+sfdx force:data:record:get -s FieldServiceMobileSettings -w "Name='Field_Service_Mobile_Settings'"
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
-sfdx force:data:record:update -s FieldServiceMobileSettings -w "Name='Field Service Mobile Settings'" -v "IsTimeSheetEnabled='TRUE'"
+sfdx force:data:record:update -s FieldServiceMobileSettings -w "Name='Field_Service_Mobile_Settings'" -v "IsTimeSheetEnabled='TRUE'"
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/page/home
