438f0f3a1728d268c3cfbc4b14df34c7d2e01f61
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-27 20:29:23 -0400

    Update orgInit.sh
    try try 2

--- orgInit.sh
+++ orgInit.sh
@@ -7,7 +7,5 @@ sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
 sfdx force:data:record:update -s FieldServiceMobileSettings -w "Name='Field Service Mobile Settings'" -v "IsTimeSheetEnabled=TRUE"
-sfdx force:data:record:update  -s  -v "ExpirationDate=2017-12-01T00:58:04.000+0000"
-sfdx force:data:record:update -s  -i 001D000000Kv3dl -v "Name=NewAcme"
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/page/home
