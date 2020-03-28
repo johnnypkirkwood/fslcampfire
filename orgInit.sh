76c278095998d59c24bedc096f84edb85c109315
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-28 11:06:47 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -6,6 +6,6 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
-sfdx force:data:record:update -s FieldServiceMobileSettings -w "DeveloperName='Field_Service_Mobile_Settings'" -v "IsTimeSheetEnabled=TRUE"
+sfdx force:data:record:update -s FieldServiceMobileSettings -w "DeveloperName=Field_Service_Mobile_Settings" -v "IsTimeSheetEnabled=TRUE"
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/page/home
