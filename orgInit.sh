ff8c57ea4c17d2119ee34cd5bdc113678075740c
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-28 13:09:15 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -6,6 +6,5 @@ sfdx force:source:push
 sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
-sfdx force:apex:execute -f setup/ApexClasses/UpdateFSMS.apex
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/page/home
