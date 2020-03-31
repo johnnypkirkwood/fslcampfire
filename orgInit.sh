a068e456fbdf0d209fcc205183fe4d0acf47807b
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-31 11:11:41 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -7,4 +7,7 @@ sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
 sfdx force:user:password:generate
-sfdx force:org:open -p /lightning/page/home
+sfdx force:apex:execute >>  User usr = [SELECT Id, TimeZoneSidKey from User WHERE Name = 'User User'];
+    usr.TimeZoneSidKey = 'America/NewYork';
+    Update usr;
+    >>sfdx force:org:open -p /lightning/page/home
