c4f948876c7227457cc535233b935ef7dc808691
johnnypkirkwood <kirkwood.johnny@gmail.com>
2020-03-31 11:17:22 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -3,10 +3,7 @@ sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
-sfdx force:apex:execute >>User usr = [SELECT Id, TimeZoneSidKey from User WHERE Name = 'User User'];
-usr.TimeZoneSidKey = 'America/NewYork';
-Update usr;
->>sfdx force:user:permset:assign -n FSL_Dispatcher_License
+sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
 sfdx force:user:password:generate
