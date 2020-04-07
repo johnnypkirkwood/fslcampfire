20125c8bfdb11b839db7f23b24ac8270b57110ba
Johnny Kirkwood <kirkwood.johnny@gmail.com>
2020-04-07 09:29:53 -0400

    Trying to go back to the big shabang

--- orgInit.sh
+++ orgInit.sh
@@ -1,10 +1,26 @@
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_License
 sfdx force:source:retrieve -m PermissionSet:FSL_Admin_Permissions
 sfdx force:source:retrieve -m PermissionSet:FSL_Dispatcher_Permissions
+sfdx force:source:retrieve -m PermissionSet:FSLAdmin2
+sfdx force:source:retrieve -m PermissionSet:FSLAgent2
+sfdx force:source:retrieve -m PermissionSet:FSLMobileLicense2
+sfdx force:source:retrieve -m PermissionSet:FSLResourceLicense2
+sfdx force:source:retrieve -m PermissionSet:FSLResourcePerms2
+sfdx force:source:retrieve -m PermissionSet:FSLSelfService2
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:permset:assign -n FSL_Dispatcher_License
 sfdx force:user:permset:assign -n FSL_Admin_Permissions
 sfdx force:user:permset:assign -n FSL_Dispatcher_Permissions
+sfdx force:user:permset:assign -n FSLAdmin2
+sfdx force:user:permset:assign -n FSLAgent2
+sfdx force:user:permset:assign -n FSLMobileLicense2
+sfdx force:user:permset:assign -n FSLResourceLicense2
+sfdx force:user:permset:assign -n FSLResourcePerms2
+sfdx force:user:permset:assign -n FSLSelfService2
 sfdx force:user:password:generate
+sfdx force:apex:execute -f SetupScripting/TimeZoneScript.cls
+sfdx force:apex:execute -f SetupScripting/EnableTimeSheets.cls
+sfdx force:apex:execute -f SetupScripting/UpdateData.cls
+sfdx force:apex:execute -f SetupScripting/LKLCron.cls
 sfdx force:org:open -p /lightning/page/home
