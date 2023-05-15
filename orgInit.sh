0d138288f902511c515c1f90696ab345a540e859
Johnny Kirkwood <kirkwood.johnny@gmail.com>
2023-05-15 15:20:09 -0400

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,4 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:source:push
 sfdx force:user:password:generate
-sfdx force:apex:execute -f SetupScripting/TimeZoneScript.cls
-sfdx force:apex:execute -f SetupScripting/EnableTimeSheets.cls
-sfdx force:apex:execute -f SetupScripting/UpdateData.cls
-sfdx force:apex:execute -f SetupScripting/LKLCron.cls
 sfdx force:org:open -p /lightning/page/home
