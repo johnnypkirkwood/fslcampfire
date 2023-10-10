983fc29fcd0fda8fa7de6c0a86929b30d3d1e232
Johnny Kirkwood <kirkwood.johnny@gmail.com>
2023-10-10 09:25:37 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -2,3 +2,4 @@ sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 sfdx force:user:password:generate
 sfdx force:org:open -p /lightning/setup/FieldServiceSettings/home
 
+hello world
